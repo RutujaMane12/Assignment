e7c5402cb9206006eb029ed878f456ae9ca0dba7
RutujaMane12 <114737315+RutujaMane12@users.noreply.github.com>
2022-10-20 12:33:14 +0530

    Add files via upload

---
+++ DS_assignments/Day_2/ass5.js
@@ -0,0 +1,59 @@
+function union(a, b) {
+    let unionSet = new Set(a);
+    for (let i of b) {
+        unionSet.add(i);
+    }
+    return unionSet
+}
+
+
+
+function intersection(setA, setB) {
+    let intersectionSet = new Set();
+
+    for (let i of setB) {
+        if (setA.has(i)) {
+            intersectionSet.add(i);
+        }
+    }
+    return intersectionSet;
+}
+
+// perform difference operation
+// elements of set a that are not in set b
+function difference(setA, setB) {
+    let differenceSet = new Set(setA)
+    for (let i of setB) {
+        differenceSet.delete(i)
+    }
+    return differenceSet
+}
+
+function subset(setA, setB) {
+    for (let i of setB) {
+        if (!setA.has(i)) {
+            return false
+        }
+    }
+    return true
+}
+
+// two sets of fruits
+const setA = new Set(['apple', 'mango', 'orange']);
+const setB = new Set(['grapes', 'apple', 'banana']);
+
+const result1 = union(setA, setB);
+
+console.log(result1);
+
+const result2 = intersection(setA, setB);
+
+console.log(result2);
+
+const result3 = difference(setA, setB);
+
+console.log(result3);
+
+const result4 = subset(setA, setB);
+
+console.log(result4);
